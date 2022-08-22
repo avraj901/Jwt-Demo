e2ef977e6679a30a425f88ac31372799be3ab694
unknown <cogjava3115@iiht.tech>
2022-08-22 09:17:02 +0530

    Added jwt response

--- src/main/java/com/token/demo/controller/HomeController.java
+++ src/main/java/com/token/demo/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,5 +41,10 @@ public class HomeController {
 		} catch (BadCredentialsException e) {
 			throw new Exception("INVALID_CREDENTIALS", e);
 		}
+		
+		final UserDetails userDetails = userService.loadUserByUsername(jwtRequest.getUserName());
+		final String token = jwtUtility.generateToken(userDetails);
+		
+		return new JwtResponse(token);
 	}
 }
